1c76682139ae8e33d5261d47752e17f2ba5faf2b
iamdefinitelyahuman <ben@wolfcrypto.net>
2018-11-02 23:05:11 +0000

    added Contract class

--- lib/components/network.py
+++ lib/components/network.py
@@ -8,27 +8,51 @@ from web3 import Web3, HTTPProvider
 
 from lib.components.config import CONFIG
 
+
+def _format_args(args):
+    if type(args[-1]) is dict:
+        return args[:-1], args[-1]
+    return args, {'from': web3.eth.accounts[0]}
+
 class Network:
 
     def __init__(self):
         pass
 
-    def deploy(self, name, *args, **kwargs):
-        if not kwargs:
-            kwargs = {'from': self.accounts[0]}
+    def deploy(self, name, *args):
+        args, tx = _format_args(args)
         interface = next(v for k,v in _compiled.items() if k.split(':')[-1] == name)
         contract = self.web3.eth.contract(
             abi = interface['abi'],
             bytecode = interface['bin']
         )
-        txid = contract.constructor(*args).transact(kwargs)
+        txid = contract.constructor(*args).transact(tx)
         txreceipt = self.web3.eth.waitForTransactionReceipt(txid)
-        setattr(self, name, self.web3.eth.contract(
-            address = txreceipt.contractAddress,
-            abi = interface['abi']
-        ))
+        setattr(self, name, Contract(txreceipt.contractAddress, interface['abi']))
         return getattr(self, name)
 
+class Contract:
+
+    def __init__(self, address, abi):
+        self._contract = web3.eth.contract(address = address, abi = abi)
+        self._abi = dict((
+            i['name'],
+            True if i['stateMutability'] in ['view','pure'] else False
+            ) for i in abi if i['type']=="function")
+    
+    def __getattr__(self, name):
+        if name not in self._abi:
+            return getattr(self._contract, name)
+        def _call(*args):
+            result = getattr(self._contract.functions,name)(*args).call()
+            if type(result) is not list:
+                return web3.toHex(result) if type(result) is bytes else result
+            return [(web3.toHex(i) if type(i) is bytes else i) for i in result]
+        def _tx(*args):
+            args, tx = _format_args(args)
+            result = getattr(self._contract.functions,name)(*args).transact(tx)
+            return web3.toHex(result)
+        return _call if self._abi[name] else _tx
 
 print("Compiling contracts...")
 _compiled = solc.compile_files(
@@ -47,5 +71,5 @@ else:
 
 if 'test-rpc' in netconf:
     rpc = Popen(netconf['test-rpc'], stdout = DEVNULL, stdin = DEVNULL)
-Network.web3 = Web3(HTTPProvider(netconf['host']))
+web3 = Network.web3 = Web3(HTTPProvider(netconf['host']))
 Network.accounts = Network.web3.eth.accounts
