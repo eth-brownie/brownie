e39fea8325977c9a000362a60044a719111af339
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-03 14:25:21 +0000

    Modified setup process and verbosity

--- lib/test.py
+++ lib/test.py
@@ -29,35 +29,29 @@ if len(sys.argv)>2 and sys.argv[2][:2]!="--":
         sys.exit("ERROR: Cannot find tests/{}.py".format(sys.argv[2]))
     test_files = [sys.argv[2]]
 else:
-    test_files = [i[:-3] for i in os.listdir('tests') if i[-3:] == ".py"]
+    test_files = [i[:-3] for i in os.listdir('tests') if i[-3:] == ".py" and i[:5] != "setup"]
     test_files.remove('__init__')
 
-if os.path.exists('tests/{}.py'.format(CONFIG['setup'])):
-    setup_module = importlib.import_module("tests.{}".format(CONFIG['setup']))
-    if not hasattr(setup_module, 'setup'):
-        sys.exit("ERROR: Setup module has no setup() function defined.")
-    print("Imported setup module from tests/{}.py".format(CONFIG['setup']))
-    test_files.remove(CONFIG['setup'])
-else:
-    setup_module = False
-    print("WARNING: No setup module found.")
+
 
 for name in test_files:
     module = importlib.import_module("tests."+name)
-    test_names = [i for i in dir(module) if i[:4]=="test" and callable(getattr(module,i))]
+    test_names = open("tests/{}.py".format(name),'r').read().split("\ndef ")[1:]
+    test_names = [i.split("(")[0] for i in test_names if i[0]!="_"]
     if not test_names:
-        print("WARNING: Module '{}' has no test functions defined.")
+        print("WARNING: Could not find any test functions in {}.py".format(name))
         continue
+    if hasattr(module, "SETUP_MODULE"):
+        setup = importlib.import_module("tests."+module.SETUP_MODULE.rstrip('.py'))
+        setup.setup(network, network.accounts)
+    print("Found {} tests in '{}'".format(len(test_names),name))
     for c,t in enumerate(test_names, start=1):
         network = Network()
-        if setup_module and not hasattr(module, 'NO_SETUP'):
-            setup_module.setup(network, network.accounts)
-        if hasattr(module, 'setup'):
-            try: module.setup(network. network.accounts)
-            except Exception as e:
-                print("ERROR: {} while running setup function in {}.".format(e, name))
-                break
-        print("Running '{}' in {} ({}/{})...".format(t,name,len(test_names),c))
+        fn = getattr(module,t)
+        if fn.__doc__:
+            print("{} ({}/{})...".format(fn.__doc__,c,len(test_names)))
+        else:
+            print("Running test '{}' ({}/{})...".format(t,c,len(test_names)))
         try:
             getattr(module,t)(network, network.accounts)
         except Exception as e:
