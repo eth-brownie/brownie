7e8be7b56e5a208f808d89ebfb1f9b99a122d419
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-04 03:37:02 +0000

    installer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if grep --quiet "alias brownie=" ~/.bash_aliases; then
     echo "Brownie is already installed."
 else
-    echo "alias brownie=\"${PWD}\"" >> ~/.bash_aliases
+    echo "alias brownie=\"python3 ${PWD}\"" >> ~/.bash_aliases
     echo "Brownie has been installed!"
 fi
