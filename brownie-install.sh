4222d1a1a2d2c214ee88b8d77f2656c0d57886c5
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-12-05 03:16:36 +0700

    installer

--- brownie-install.sh
+++ brownie-install.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
+
 cd /usr/local/lib
 git clone --depth=1 https://github.com/iamdefinitelyahuman/brownie
 chmod -R 777 /usr/local/lib/brownie
 cd brownie
 virtualenv -p /usr/bin/python3.7 env
-source env/bin/activate
-pip install -r requirements.txt
-deactivate
+env/bin/pip install -r requirements.txt
 
 echo '#!/bin/bash' > /usr/local/bin/brownie
 echo 'source /usr/local/lib/brownie/env/bin/activate' >> /usr/local/bin/brownie
 echo 'python /usr/local/lib/brownie "$@"' >> /usr/local/bin/brownie
-echo 'deactivate' >> /usr/local/bin/brownie
 chmod +x /usr/local/bin/brownie
