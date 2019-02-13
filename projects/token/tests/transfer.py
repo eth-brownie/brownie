af00e373cfa510d5e8d5c8bda9bb1a3b4973270c
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-02-13 03:49:14 +0200

    updated tests in projects/token

--- projects/token/tests/transfer.py
+++ projects/token/tests/transfer.py
@@ -10,6 +10,14 @@ def transfer():
     '''Transfer tokens'''
     token = Token[0]
     check.equal(token.totalSupply(), "1000 ether", "totalSupply is wrong")
-    token.transfer(accounts[1], "0.1 ether", {'from':accounts[0]})
-    check.equal(token.balanceOf(accounts[1]), "0.1 ether", "Accounts 1 balance is wrong")
-    check.equal(token.balanceOf(accounts[0]), "999.9 ether", "Accounts 0 balance is wrong")
+    token.transfer(accounts[1], "0.1 ether", {'from': accounts[0]})
+    check.equal(
+        token.balanceOf(accounts[1]),
+        "0.1 ether",
+        "Accounts 1 balance is wrong"
+    )
+    check.equal(
+        token.balanceOf(accounts[0]),
+        "999.9 ether",
+        "Accounts 0 balance is wrong"
+    )
