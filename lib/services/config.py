0f8ee573175275cdba3abdca533cb1e6462c7a33
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-02-18 23:38:36 +0200

    modify how config is handled

--- lib/services/config.py
+++ lib/services/config.py
@@ -4,6 +4,8 @@ import json
 import os
 import sys
 
+
+# modifies config network settings
 def set_network(name):
     try:
         CONFIG['active_network'] = CONFIG['networks'][name]
@@ -17,6 +19,41 @@ def set_network(name):
         raise KeyError("Network '{}' is not defined in config.json".format(name))
 
 
+# merges project .json with brownie .json 
+def _recursive_update(original, new):
+    for k in new:
+        if type(new[k]) is dict and k in original:
+            _recursive_update(original[k], new[k])
+        else: original[k] = new[k]
+
+
+# dictionary that returns False if key does not exist
+class FalseyDict:
+
+    def __init__(self):
+        self._dict = {}
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+
+    def __getitem__(self, key):
+        if key in self._dict:
+            return self._dict[key]
+        return False
+
+
+# move argv options into FalseyDict
+ARGV = FalseyDict()
+for key in [i for i in sys.argv if i[:2]=="--"]:
+    idx = sys.argv.index(key)
+    if len(sys.argv) >= idx+2 and sys.argv[idx+1][:2] != "--":
+        ARGV[key] = sys.argv[idx+1]
+    else:
+        ARGV[key] = True
+ARGV['mode'] = "console" if sys.argv[1] == "console" else "script"
+
+
+# set folders
 folder = sys.modules['__main__'].__file__
 folder = folder[:folder.rfind("/")]
 sys.path.insert(0, folder)
@@ -25,7 +62,6 @@ CONFIG['folders'] = {
     'brownie': folder,
     'project': os.path.abspath('.')
 }
-
 folders = os.path.abspath('.').split('/')
 for i in range(len(folders),0,-1):
     folder = '/'.join(folders[:i])
@@ -34,12 +70,11 @@ for i in range(len(folders),0,-1):
         break
 sys.path.insert(1, ".")
 
-if os.path.exists("brownie-config.json"):
-    for k,v in json.load(open("brownie-config.json", 'r')).items():
-        if type(v) is dict and k in CONFIG:
-            CONFIG[k].update(v)
-        else: CONFIG[k] = v
 
+# update config
+if os.path.exists("brownie-config.json"):
+    local_conf = json.load(open("brownie-config.json", 'r'))
+    _recursive_update(CONFIG, local_conf)
 try:
     CONFIG['logging'] = CONFIG['logging'][sys.argv[1]]
     CONFIG['logging'].setdefault('tx',0)
@@ -49,7 +84,9 @@ try:
 except:
     CONFIG['logging'] = {"tx":1 ,"exc":1}
 
-if '--network' in sys.argv:
-    set_network(sys.argv[sys.argv.index('--network')+1])
+
+# set network
+if ARGV['network']:
+    set_network(ARGV['network'])
 else:
     set_network(CONFIG['network_defaults']['name'])
