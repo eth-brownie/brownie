bab8b2d4df8c14ed06cb748d868f3af1d74fa7a6
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-04-08 03:38:36 +0300

    back to __add__ in main

--- brownie/__init__.py
+++ brownie/__init__.py
@@ -13,5 +13,22 @@ from brownie.project import (
 )
 from brownie.test import check
 import brownie.config as _config
+from brownie.utils.compiler import compile_source
+from brownie.utils import alert
+from brownie.types.convert import wei
+
+__all__ = [
+    'accounts',
+    'history',
+    'network',
+    'rpc',
+    'web3',
+    'project',
+    '__project',
+    'check',
+    'compile_source',
+    'alert',
+    'wei'
+]
 
 CONFIG = _config.CONFIG
