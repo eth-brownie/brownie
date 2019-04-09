f4423b283ef6313269e94f98b4adf94526fb88c1
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-04-09 09:23:00 +0400

    remove projects

--- projects/metacoin/scripts/deploy_contracts.py
+++
@@ -1,7 +0,0 @@
-
-from brownie import *
-
-
-def main():
-    ConvertLib.deploy(accounts[0])
-    MetaCoin.deploy(accounts[0])
--- projects/token/scripts/token.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-from brownie import *
-
-def main():
-    accounts[0].deploy(Token, "Test Token", "TEST", 18, "1000 ether")
