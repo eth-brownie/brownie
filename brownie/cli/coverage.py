252d07f897b0acb9441d4fa579b98dc4740fba1c
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-04-17 13:56:50 +1000

    cli wip (almost there)

--- brownie/cli/coverage.py
+++ brownie/cli/coverage.py
@@ -9,8 +9,7 @@ import json
 from brownie.cli.test import get_test_files, run_test
 from brownie.cli.utils import color
 import brownie.network as network
-from brownie.test.coverage import get_coverage_map
-from brownie.utils.compiler import compile_contracts
+from brownie.utils.compiler import get_build
 import brownie._config as config
 
 CONFIG = config.CONFIG
@@ -55,10 +54,6 @@ def main():
     else:
         idx = slice(0, None)
 
-    compiled = compile_contracts(
-        Path(CONFIG['folders']['project']).joinpath('contracts')
-    )
-    fn_map_original = get_coverage_map(compiled)
     network.connect(config.ARGV['network'], True)
 
     if args['--always-transact']:
@@ -75,7 +70,8 @@ def main():
                 "\n{0[error]}ERROR{0}: Cannot ".format(color) +
                 "calculate coverage while tests are failing\n"
             )
-        fn_map = deepcopy(fn_map_original)
+        coverage_map = {}
+        coverage_eval = {}
         for tx in history:
             if not tx.receiver:
                 continue
@@ -86,19 +82,22 @@ def main():
                 source = t['source']['filename']
                 if not name or not source:
                     continue
+                if name not in coverage_map:
+                    coverage_map[name] = get_build(name)['coverageMap']
+                    coverage_eval[name] = dict((i,{}) for i in coverage_map[name])
                 try:
                     # find the function map item and record the tx
                     
-                    fn = next(v for k,v in fn_map['contracts'][name][source].items() if pc in v['fn']['pc'])
-                    print(fn)
-                    fn['fn']['tx'].add(tx)
-                    fn_map['counts'][name] += 1
+                    fn = next(v for k,v in coverage_map[name][source].items() if pc in v['fn']['pc'])
+                    fn['fn'].setdefault('tx',set()).add(tx)
                     if t['op']!="JUMPI":
                         # if not a JUMPI, find the line map item and record
-                        next(i for i in fn['line'] if pc in i['pc'])['tx'].add(tx)
+                        next(i for i in fn['line'] if pc in i['pc']).setdefault('tx',set()).add(tx)
                         continue
                     # if a JUMPI, we need to have hit the jump pc AND a related opcode
                     ln = next(i for i in fn['line'] if pc==i['jump'])
+                    for key in ('tx', 'true', 'false'):
+                        ln.setdefault(key, set())
                     if tx not in ln['tx']:
                         continue
                     # if the next opcode is not pc+1, the JUMPI was executed truthy
@@ -108,78 +107,60 @@ def main():
                 except StopIteration:
                     continue
 
-        for contract in [k for k,v in fn_map['counts'].items() if not v]:
-            del fn_map['contracts'][contract]
-        del fn_map['counts']
-        
-        for contract, source, fn_name, maps in [(k,w,y,z) for k,v in fn_map['contracts'].items() for w,x in v.items() for y,z in x.items()]:
+
+        for contract, source, fn_name, maps in [(k,w,y,z) for k,v in coverage_map.items() for w,x in v.items() for y,z in x.items()]:
 
             fn = maps['fn']
+            if 'tx' not in fn or not fn['tx']:
+                coverage_eval[contract][source][fn_name] = {'pct':0}
+                continue
             for ln in maps['line']:
+                if 'tx' not in ln:
+                    ln['count'] = 0
+                    continue
                 if ln['jump']:
                     ln['jump'] = [len(ln.pop('true')), len(ln.pop('false'))]
                 ln['count'] = len(ln.pop('tx'))
-                del ln['pc']
 
-            fn['count'] = len(fn.pop('tx'))
-            del fn['pc']
-            if not fn['count'] or not [i for i in maps['line'] if i['count']]:
-                del maps['line']
-                fn['coverage'] = {'pct':0}
+            if not [i for i in maps['line'] if i['count']]:
+                coverage_eval[contract][source][fn_name] = {'pct':0}
                 continue
 
             count = 0
-            fn['coverage'] = {'line':[], 'true':[], 'false':[]}
+            coverage = {'line':[], 'true':[], 'false':[]}
             for c,i in enumerate(maps['line']):
                 if not i['count']:
                     continue
                 if not i['jump'] or False not in i['jump']:
-                    fn['coverage']['line'].append(c)
+                    coverage['line'].append(c)
                     count+=2 if i['jump'] else 1
                     continue
                 if i['jump'][0]:
-                    fn['coverage']['true'].append(c)
+                    coverage['true'].append(c)
                     count+=1
                 if i['jump'][1]:
-                    fn['coverage']['false'].append(c)
+                    coverage['false'].append(c)
                     count+=1
-            total = sum([1 if not i['jump'] else 2 for i in maps['line']])
-            pct = count / total
-            if count == total:
-                del maps['line']
-                fn['coverage'] = {'pct':1}
+            pct = count / maps['total']
+            if count == maps['total']:
+                coverage_eval[contract][source][fn_name] = {'pct': 1}
             else:
-                fn['coverage']['pct']=round(count/total,2)
-        
-        # for contract in fn_map:
-        #     for fn in fn_map[contract].copy():
-        #         fn['count'] = len(fn.pop('tx'))
-        #         del fn['pc']
-        #         line_fn = [i for i in line_map[contract] if i['method']==fn['method']]
-        #         if not fn['count'] or not [i for i in line_fn if i['count']]:
-        #             for ln in line_fn:
-        #                 line_map[contract].remove(ln)
-        #         elif line_fn:
-        #             fn_map[contract].remove(fn)
-        #     fn_map[contract].extend(line_map[contract])
+                coverage['pct']=round(count/maps['total'],2)
+                coverage_eval[contract][source][fn_name] = coverage
 
-        # for contract in list(fn_map):
-        #     fn_list = sorted(set(i['method'] for i in fn_map[contract] if i['method']))
-        #     if not fn_list or not [i for i in fn_map[contract] if i['count']]:
-        #         del fn_map[contract]
         path = Path(CONFIG['folders']['project'])
         path = path.joinpath("build/coverage"+filename[5:]+".json")
         for p in list(path.parents)[::-1]:
             if not p.exists():
                 p.mkdir()
         json.dump(
-            fn_map,
+            coverage_eval,
             path.open('w'),
             sort_keys=True,
             indent=4
         )
 
-    exit()
+    # TODO - beyond here things are still broken
     print("\nCoverage analysis complete!\n")
     for contract in fn_map:
         fn_list = sorted(set(i['method'] for i in fn_map[contract] if i['method']))
