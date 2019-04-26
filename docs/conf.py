8118bf632c7babd802e61dc87c1e841752f796c0
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-04-26 17:56:33 +0400

    update changelog, bump version to 1.0.0b3

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Benjamin Hauser'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.0b2'
+release = '1.0.0b3'
 
 
 # -- General configuration ---------------------------------------------------
