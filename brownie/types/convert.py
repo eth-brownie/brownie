da8c6eb8ed6fc875a829ea70f6abad857713b139
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-05-03 01:19:34 +0300

    add conversion methods

--- brownie/types/convert.py
+++ brownie/types/convert.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import eth_utils
+
 UNITS = {
     'kwei': 3,
     'babbage': 3,
@@ -33,16 +35,16 @@ def format_input(abi, inputs):
             name, ",".join(types)
         ))
     for i, type_ in enumerate(types):
-        if type_[-1] == "]":
+        if ']' in type_:
             # input value is an array, have to check every item
-            t, length = type_.rstrip(']').rsplit('[', maxsplit=1)
+            base_type, length = type_[:-1].rsplit('[', maxsplit=1)
             if length != "" and len(inputs[i]) != int(length):
                 raise ValueError(
                     "'{}': Argument {}, sequence has a ".format(name, i) +
                     "length of {}, should be {}".format(len(inputs[i]), type_)
                     )
             inputs[i] = format_input(
-                {'name': name, 'inputs':[{'type': t}] * len(inputs[i])},
+                {'name': name, 'inputs':[{'type': base_type}] * len(inputs[i])},
                 inputs[i]
             )
             continue
@@ -65,6 +67,70 @@ def format_input(abi, inputs):
                     name, i, type(inputs[i]).__name__, inputs[i], type_))
     return inputs
 
+
+def _check_int_size(type_):
+    size = int(type_.strip("uint") or 256)
+    if size < 8 or size > 256 or size // 8 != size / 8:
+        raise ValueError("Invalid type: {}".format(type_))
+    return size
+
+def to_uint(value, type_="uint256"):
+    value = wei(value)
+    size = _check_int_size(type_)
+    if value < 0 or value >= 2**int(size):
+        raise OverflowError("{} is outside allowable range for {}".format(value, type_))
+
+def to_int(value, type_="int256"):
+    value = wei(value)
+    size = _check_int_size(type_)
+    if value < -2**int(size) // 2 or value >= 2**int(size) // 2:
+        raise OverflowError("{} is outside allowable range for {}".format(value, type_))
+
+def to_bool(value):
+    if value not in (0, 1, True, False):
+        raise TypeError("Cannot convert '{}' to bool".format(value))
+    return bool(value)
+
+def to_address(value):
+    if type(value) is bytes:
+        value = value.hex()
+    value = eth_utils.add_0x_prefix(str(value))
+    try:
+        return eth_utils.to_normalized_address(value)
+    except ValueError:
+        raise ValueError("{} is not a valid ETH address.")
+
+def to_bytes(value, type_="bytes32")
+    if type(value) not in (bytes, str):
+        raise ValueError("{} cannot be converted to {}".format(value, type_))
+    if type_ == "byte":
+        type_ = "bytes1"
+    if type_ != "bytes":
+        size = type_.strip("bytes")
+        if size < 1 or size > 32:
+            raise ValueError("Invalid type: {}".format(type_))
+    else:
+        size = float('inf')
+    if type(value) is bytes:
+        if len(eth_utils.to_hex(value)) - 2 > size:
+            raise OverflowError("{} exceeds maximum length for {}".format(value, type_))
+        return value
+    if type(value) is str:
+        if not eth_utils.is_hex(value):
+            raise ValueError("{} is not a valid hex string, cannot convert to {}".format(type_))
+        value = eth_utils.add_0x_prefix(value)
+        if type_ == "bytes":
+            return eth_utils.to_bytes(value)
+        try:
+            return int(value, 16).to_bytes(size, "big")
+        except OverflowError:
+            raise OverflowError("{} exceeds maximum length for {}".format(value, type_))
+
+def to_string(value):
+    value = str(value)
+    if eth_utils.is_hex(value):
+        value = eth_utils.to_text(value)
+    return value.encode()
 
 
 def format_output(value):
