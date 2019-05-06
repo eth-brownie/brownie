91a0a6d48edfaaaf1cca66a0c900ea4a86cf5e7d
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-05-06 15:51:19 +0300

    add RPC exceptions

--- brownie/exceptions.py
+++ brownie/exceptions.py
@@ -14,6 +14,31 @@ class ExpectedFailing(Exception):
     pass
 
 
+class _RPCBaseException(Exception):
+
+    def __init__(self, msg, cmd, proc, uri):
+        code = proc.poll()
+        out = proc.stdout.read().decode().strip() or "  (Empty)"
+        err = proc.stderr.read().decode().strip() or "  (Empty)"
+        super().__init__(
+            "{}\n\nCommand: {}\nURI: {}\nExit Code: {}\n\nStdout:\n{}\n\nStderr:\n{}".format(
+                msg, cmd, uri, code, out, err
+            )
+        )
+
+class RPCProcessError(_RPCBaseException):
+
+    def __init__(self, cmd, proc, uri):
+        super().__init__("Unable to launch local RPC client.", cmd, proc, uri)
+
+
+class RPCConnectionError(_RPCBaseException):
+
+    def __init__(self, cmd, proc, uri):
+        super().__init__("Able to launch RPC client, but unable to connect.", cmd, proc, uri)
+
+
+
 class VirtualMachineError(Exception):
 
     '''Raised when a call to a contract causes an EVM exception.
