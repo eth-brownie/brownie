34a1324ee08934a1902bab54cb130b62a3d9a9b4
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-06-01 01:58:05 +0100

    branch and statement maps - perfect! but soooo slow

--- brownie/project/compiler.py
+++ brownie/project/compiler.py
@@ -5,6 +5,7 @@ from collections import deque
 from hashlib import sha1
 from pathlib import Path
 import solcast
+from solcast.utils import is_inside_offset
 import solcx
 
 from . import sources
@@ -82,12 +83,22 @@ def _compile_and_format(input_json):
     source_nodes = solcast.from_standard_output(deepcopy(output_json))
 
     for path, contract_name in [(k, v) for k in path_list for v in output_json['contracts'][k]]:
+
         evm = output_json['contracts'][path][contract_name]['evm']
         node = next(i[contract_name] for i in source_nodes if i.name == path)
         bytecode = format_link_references(evm)
 
+        pc_list = generate_pc_list(
+            evm['deployedBytecode']['sourceMap'],
+            evm['deployedBytecode']['opcodes'],
+            node
+        )
+
+        paths = sorted([node.parent.path]+[i.parent.path for i in node.dependencies])
+
         build_json[contract_name] = {
             'abi': output_json['contracts'][path][contract_name]['abi'],
+            'allSourcePaths': paths,
             'ast': output_json['sources'][path]['ast'],
             'bytecode': bytecode,
             'bytecodeSha1': sha1(bytecode[:-68].encode()).hexdigest(),
@@ -100,26 +111,25 @@ def _compile_and_format(input_json):
             'fn_offsets': [[node.name+'.'+i.name, i.offset] for i in node.functions],
             'offset': node.offset,
             'opcodes': evm['deployedBytecode']['opcodes'],
+            'pcMap': pc_list_to_map(pc_list),
             'sha1': sources.get_hash(contract_name),
             'source': input_json['sources'][path]['content'],
             'sourceMap': evm['bytecode']['sourceMap'],
             'sourcePath': path,
             'type': node.type
         }
-
-    pc_list = generate_pc_list(source_nodes, build_json)
-    for name, value in pc_list.items():
-        build_json[name]['pcMap'] = pc_list_to_map(value)
-
-    build_json = _generate_coverageMap(build_json)
-    for data in build_json.values():
-        data['coverageMapTotals'] = _generate_coverageMapTotals(data['coverageMap'])
     return build_json
 
 
-def generate_pc_list(source_nodes, build_json):
+def generate_pc_list(source_map, opcodes, contract_node):
     '''
-    Generates an expanded sourceMap useful for debugging.
+    Generates an expanded sourceMap useful for debugging. Values that would be
+    False are not included.
+
+    Arguments:
+        source_nodes: List of SourceUnit objects
+        build_json:
+
     [{
         'path': relative path of the contract source code
         'jump': jump instruction as supplied in the sourceMap (-,i,o)
@@ -130,41 +140,37 @@ def generate_pc_list(source_nodes, build_json):
         'value': value of the instruction, if any
     }, ... ]
     '''
-    id_map = dict((x.contract_id, x) for i in source_nodes for x in i)
-    pc_list = {}
-    for name, data in build_json.items():
-        if not data['deployedBytecode']:
-            pc_list[name] = {}
+    if not opcodes:
+        return []
+
+    source_map = deque(expand_source_map(source_map))
+    opcodes = deque(opcodes.split(" "))
+    id_map = dict((i.contract_id, i) for i in [contract_node]+contract_node.dependencies)
+    pc_list = []
+    pc = 0
+
+    while source_map:
+        source = source_map.popleft()
+        pc_list.append({'op': opcodes.popleft(), 'pc': pc})
+        pc += 1
+        if source[3] != "-":
+            pc_list[-1]['jump'] = source[3]
+        if opcodes[0][:2] == "0x":
+            pc_list[-1]['value'] = opcodes.popleft()
+            pc += int(pc_list[-1]['op'][4:])
+        if source[2] == -1:
             continue
+        node = id_map[source[2]]
+        pc_list[-1]['path'] = node.parent.path
+        if source[0] == -1:
+            continue
+        offset = [source[0], source[0]+source[1]]
+        pc_list[-1]['offset'] = offset
+        try:
+            pc_list[-1]['fn'] = node.child_by_offset(offset).full_name
+        except KeyError:
+            pass
 
-        opcodes = deque(data['opcodes'].split(" "))
-        source_map = deque(expand_source_map(data['deployedSourceMap']))
-        pc = 0
-        pcMap = []
-
-        while source_map:
-            source = source_map.popleft()
-            pcMap.append({'op': opcodes.popleft(), 'pc': pc})
-            pc += 1
-            if source[3] != "-":
-                pcMap[-1]['jump'] = source[3]
-            if opcodes[-1][:2] == "0x":
-                pcMap[-1]['value'] = opcodes.popleft()
-                pc += int(pcMap[-1]['op'][4:])
-            if source[2] == -1:
-                continue
-            node = id_map[source[2]]
-            pcMap[-1]['path'] = node.parent.path
-            if source[0] == -1:
-                continue
-            offset = [source[0], source[0]+source[1]]
-            pcMap[-1]['offset'] = offset
-            try:
-                pcMap[-1]['fn'] = node.child_by_offset(offset).full_name
-            except KeyError:
-                pass
-
-        pc_list[name] = pcMap
     return pc_list
 
 
@@ -174,7 +180,7 @@ def pc_list_to_map(pc_list):
 
 
 def expand_source_map(source_map):
-    '''Expands the compressed sourceMap supplied by solc into a list of lists.''' 
+    '''Expands the compressed sourceMap supplied by solc into a list of lists.'''
     source_map = [_expand_row(i) if i else None for i in source_map.split(';')]
     for i in range(1, len(source_map)):
         if not source_map[i]:
@@ -191,151 +197,88 @@ def _expand_row(row):
     return [int(i) if i else None for i in row[:3]] + row[3:] + [None]*(4-len(row))
 
 
-def _generate_coverageMap(build_json):
-    """Adds coverage data to a build json.
-
-    A new key 'coverageMap' is created, structured as follows:
-
-    {
-        "/path/to/contract/file.sol": {
-            "functionName": [{
-                'jump': pc of the JUMPI instruction, if it is a jump
-                'start': source code start offest
-                'stop': source code stop offset
-            }],
-        }
-    }
-
-    Relevent items in the pcMap also have a 'coverageIndex' added that corresponds
-    to an entry in the coverageMap."""
-    for build in build_json.values():
-        line_map = _isolate_lines(build)
-        if not line_map:
-            build['coverageMap'] = {}
-            continue
-
-        final = dict((i, {}) for i in set(i['path'] for i in line_map))
-        for i in line_map:
-            fn = get_fn(build_json, i['path'], (i['start'], i['stop']))
-            if not fn:
-                continue
-            final[i['path']].setdefault(fn, []).append({
-                'jump': i['jump'],
-                'offset': (i['start'], i['stop'])
-            })
-            for pc in i['pc']:
-                build['pcMap'][pc]['coverageIndex'] = len(final[i['path']][fn]) - 1
-        build['coverageMap'] = final
-    return build_json
-
-
-def _generate_coverageMapTotals(coverage_map):
-    totals = {'total': 0}
-    for path, fn_name in [(k, x) for k, v in coverage_map.items() for x in v]:
-        maps = coverage_map[path][fn_name]
-        count = len([i for i in maps if not i['jump']]) + len([i for i in maps if i['jump']])*2
-        totals[fn_name] = count
-        totals['total'] += count
-    return totals
-
-
-def _isolate_lines(compiled):
-    '''Identify line based coverage map items.
-
-    For lines where a JUMPI is not present, coverage items will merge
-    to include as much of the line as possible in a single item. Where a
-    JUMPI is involved, no merge will happen and overlapping non-jump items
-    are discarded.'''
-    pcMap = compiled['pcMap']
-    line_map = {}
-
-    # find all the JUMPI opcodes
-    for i in [k for k, v in pcMap.items() if 'path' in v and v['op'] == "JUMPI"]:
-        op = pcMap[i]
-        if op['path'] not in line_map:
-            line_map[op['path']] = []
-        # if followed by INVALID or the source contains public, ignore it
-        if pcMap[i+1]['op'] == "INVALID" or " public " in _get_source(op):
+def get_statement_map(contract_node, pc_list):
+    if not pc_list:
+        return []
+    pc_list = [i for i in pc_list if 'offset' in i and i['path'] == contract_node.parent.path]
+    statement_map = []
+    for node in contract_node.children(include_parents=False, node_class="Statement"):
+        if node.root(2).name == "<constructor>":
             continue
         try:
-            # JUMPI is to the closest previous opcode that has
-            # a different source offset and is not a JUMPDEST
-            pc = next(
-                x for x in range(i - 4, 0, -1) if x in pcMap and
-                'path' in pcMap[x] and pcMap[x]['op'] != "JUMPDEST" and
-                pcMap[x]['offset'] != op['offset']
-            )
+            pc = next(i['pc'] for i in pc_list if is_inside_offset(i['offset'], node.offset))
         except StopIteration:
             continue
-        line_map[op['path']].append(_base(pc, pcMap[pc]))
-        line_map[op['path']][-1].update({'jump': i})
+        statement_map.append([pc, node.offset])
+    return statement_map
 
-    # analyze all the opcodes
-    for pc, op in [(i, pcMap[i]) for i in sorted(pcMap) if 'path' in pcMap[i]]:
-        # ignore code that spans multiple lines
-        if ';' in _get_source(op):
-            continue
-        if op['path'] not in line_map:
-            line_map[op['path']] = []
-        # find existing related coverage map item, make a new one if none exists
-        try:
-            ln = next(
-                i for i in line_map[op['path']] if
-                i['path'] == op['path'] and
-                i['start'] <= op['offset'][0] < i['stop']
-            )
-        except StopIteration:
-            line_map[op['path']].append(_base(pc, op))
-            continue
-        if op['offset'][1] > ln['stop']:
-            # if coverage map item is a jump, do not modify the source offsets
-            if ln['jump']:
-                continue
-            ln['stop'] = op['offset'][1]
-        ln['pc'].add(pc)
 
-    # sort the coverage map and merge overlaps where possible
-    for contract in line_map:
-        line_map[contract] = sorted(
-            line_map[contract],
-            key=lambda k: (k['path'], k['start'], k['stop'])
+def _get_jump(pc_list, offset, reverse=False):
+    iterable = reversed(pc_list) if reverse else pc_list
+    pc = next((i['pc'] for i in iterable if i['offset'] == offset), None)
+    if pc is None:
+        iterable = reversed(pc_list) if reverse else pc_list
+        pc = next((i['pc'] for i in iterable if is_inside_offset(i['offset'], offset)), None)
+    if pc is None:
+        return None
+    return next(i['pc'] for i in pc_list if i['pc'] > pc and i['op'] == "JUMPI")
+
+
+def get_branch_map(contract_node, pc_list):
+    if not pc_list:
+        return []
+    pc_list = [i for i in pc_list if 'offset' in i and i['path'] == contract_node.parent.path]
+    branch_map = []
+
+    # require branches
+    for node in contract_node.children(node_type="FunctionCall", name="require"):
+        binary = node.children(include_parents=False, node_type="BinaryOperation", type="bool")
+        if not binary:
+            pc = _get_jump(pc_list, node.offset)
+            if pc:
+                branch_map.append([pc, node.arguments[0].offset])
+            continue
+        for i in binary:
+            pc = _get_jump(pc_list, i.offset, True)
+            if pc:
+                branch_map.append([pc, i.offset])
+
+    # if statement branches
+    for node in contract_node.children(node_type="IfStatement"):
+
+        all_binaries = node.condition.children(
+            include_parents=True,
+            include_self=True,
+            node_type="BinaryOperation",
+            type="bool"
         )
-        ln_map = line_map[contract]
-        i = 0
-        while True:
-            if len(ln_map) <= i + 1:
-                break
-            if ln_map[i]['jump']:
-                i += 1
+
+        if not all_binaries:
+            pc = _get_jump(pc_list, node.offset)
+            if pc:
+                branch_map.append([pc, node.condition.offset])
+            continue
+
+        for i in all_binaries:
+            if not i.children(include_self=False, node_type="BinaryOperation", type="bool"):
+                pc = _get_jump(pc_list, i.offset)
+                if pc:
+                    branch_map.append([pc, i.offset])
                 continue
-            # JUMPI overlaps cannot merge
-            if ln_map[i+1]['jump']:
-                if ln_map[i]['stop'] > ln_map[i+1]['start']:
-                    del ln_map[i]
-                else:
-                    i += 1
-                continue
-            if ln_map[i]['stop'] >= ln_map[i+1]['start']:
-                ln_map[i]['pc'] |= ln_map[i+1]['pc']
-                ln_map[i]['stop'] = max(ln_map[i]['stop'], ln_map[i+1]['stop'])
-                del ln_map[i+1]
-                continue
-            i += 1
-    return [x for v in line_map.values() for x in v]
+            for x in i:
+                if x.children(include_self=True, node_type="BinaryOperation", type="bool"):
+                    continue
+                pc = _get_jump(pc_list, x.offset)
+                if pc:
+                    branch_map.append([pc, x.offset])
 
+    # ternery operator branches
+    for node in contract_node.children(node_type="Conditional"):
+        pc = _get_jump(pc_list, node.condition.offset, True)
+        if pc:
+            branch_map.append([pc, node.condition.offset])
 
-def _get_source(op):
-    return sources.get(op['path'])[op['offset'][0]:op['offset'][1]]
-
-
-def _base(pc, op):
-    return {
-        'path': op['path'],
-        'start': op['offset'][0],
-        'stop': op['offset'][1],
-        'pc': set([pc]),
-        'jump': False
-    }
+    return branch_map
 
 
 def format_link_references(evm):
