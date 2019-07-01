9602efa8335a5828218734f081a8696e677238da
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-07-01 17:31:54 +0400

    improved test verbosity

--- brownie/test/executor.py
+++ brownie/test/executor.py
@@ -8,7 +8,7 @@ from . import pathutils, loader
 from .output import TestPrinter
 from brownie.network import history, rpc
 from brownie.cli.utils import color, notify
-from brownie._config import ARGV
+from brownie._config import ARGV, CONFIG
 from brownie.test import coverage
 from brownie.exceptions import ExpectedFailing
 import brownie.network as network
@@ -35,11 +35,8 @@ def run_test_modules(test_paths, only_update=True, check_coverage=False, save=Tr
             notify("WARNING", "No tests were found.")
         return
     TestPrinter.set_grand_total(len(test_data))
-    print("Running {} tests across {} module{}.".format(
-        sum([len([x for x in i[2] if x[0].__name__ != "setup"]) for i in test_data]),
-        len(test_data),
-        "s" if len(test_data) > 1 else ""
-    ))
+    count = sum([len([x for x in i[2] if x[0].__name__ != "setup"]) for i in test_data])
+    print(f"Running {count} test{_s(count)} across {len(test_data)} module{_s(len(test_data))}.")
 
     if not network.is_connected():
         network.connect()
@@ -47,12 +44,14 @@ def run_test_modules(test_paths, only_update=True, check_coverage=False, save=Tr
         ARGV['always_transact'] = True
 
     traceback_info = []
+    failed = 0
     start_time = time.time()
     try:
         for (test_path, old_coverage_eval, method_data) in test_data:
             tb, coverage_eval, contracts = _run_module(test_path, method_data, check_coverage)
             if tb:
                 traceback_info += tb
+                failed += 1
             if not save:
                 continue
 
@@ -76,14 +75,12 @@ def run_test_modules(test_paths, only_update=True, check_coverage=False, save=Tr
     finally:
         if check_coverage:
             del ARGV['always_transact']
-        print("\nTotal runtime: {:.4f}s".format(time.time() - start_time))
+        print(f"\nTotal runtime: {time.time() - start_time:.4f}s")
         if traceback_info:
-            notify("WARNING", "{} test{} failed".format(
-                len(traceback_info),
-                "s" if len(traceback_info) > 1 else ""
-            ))
+            count = len(traceback_info)
+            notify("WARNING", f"{count} test{_s(count)} failed in {failed} module{_s(failed)}.")
             for err in traceback_info:
-                print("\nTraceback for {0[0]}:\n{0[1]}".format(err))
+                print(f"\nTraceback for {err[0]}:\n{err[1]}")
 
 
 def _get_data(test_paths, only_update, check_coverage):
@@ -108,7 +105,7 @@ def _run_module(test_path, method_data, check_coverage):
 
     has_setup = method_data[0][0].__name__ == "setup"
     printer = TestPrinter(
-        test_path,
+        str(test_path).replace(CONFIG['folders']['project'], "."),
         0 if has_setup else 1,
         len(method_data) - (1 if has_setup else 0)
     )
@@ -158,7 +155,7 @@ def _run_method(fn, args, coverage_eval, printer, check_coverage):
         printer_args = [e, args['pending']]
         if not args['pending'] or type(e) == ExpectedFailing:
             traceback_info = [(
-                "{0[module]}{1.__module__}.{0[callable]}{1.__name__}{0}".format(color, fn),
+                f"{color['module']}{fn.__module__}.{color['callable']}{fn.__name__}{color}",
                 color.format_tb(sys.exc_info(), sys.modules[fn.__module__].__file__),
                 type(e)
             )]
@@ -173,3 +170,7 @@ def _get_contracts():
         i.contract_address for i in history if type(i.contract_address) is Contract and
         not i.contract_address._build['sourcePath'].startswith('<string')
     )
+
+
+def _s(count):
+    return "s" if count != 1 else ""
