827cc95880b78fb06a1eb9fba3c98907bfa3b065
Crawford Leeds <crawford@cocoa.me>
2019-09-01 21:25:56 -0600

    add typing for brownie/_singleton.py

--- brownie/_singleton.py
+++ brownie/_singleton.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
+from typing import Dict, Any, Tuple
 
 class _Singleton(type):
 
-    _instances = {}
+    _instances: Dict[_Singleton, Any] = {}
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls, *args: Tuple, **kwargs: Dict) -> Any:
         if cls not in cls._instances:
             cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
