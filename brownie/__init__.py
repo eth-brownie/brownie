9c9e74c3569e65dafc2dd7f627b68d9aeb8f5caf
Crawford Leeds <crawford@crawfordleeds.com>
2019-09-11 23:48:35 -0600

    add some more comments

--- brownie/__init__.py
+++ brownie/__init__.py
@@ -17,12 +17,12 @@ from brownie._config import CONFIG as config
 from brownie.convert import Wei
 
 __all__ = [
-    'accounts',
+    'accounts', # accounts is an Accounts singleton
     'alert',
     'history',  # history is a TxHistory singleton
     'network',
-    'rpc',
-    'web3',
+    'rpc', # rpc is a Rpc singleton
+    'web3', # web3 is a Web3 singleton
     'project',
     'compile_source',
     'run',
