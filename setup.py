e35e80b9848eafe321cdd27e7617e39d2efda3e7
Crawford Leeds <crawford@crawfordleeds.com>
2019-11-25 14:07:14 -0700

    update development status to production/stable in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     include_package_data=True,
     python_requires=">=3.6,<4",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
