60ed067bb67f758dea3f2ebdb65513865e69d9b9
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-01-07 21:35:47 +0100

    modify travis builds, update changelog, bump version

--- brownie/_cli/__main__.py
+++ brownie/_cli/__main__.py
@@ -11,7 +11,7 @@ from brownie._config import ARGV
 from brownie.exceptions import ProjectNotFound
 from brownie.utils import color, notify
 
-__version__ = "1.3.2"
+__version__ = "1.4.0"
 
 __doc__ = """Usage:  brownie <command> [<args>...] [options <args>]
 
