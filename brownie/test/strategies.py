38159bad72fc4b03d40cdd39c979b234e3238872
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-01-24 03:08:18 +0400

    add array strategy

--- brownie/test/strategies.py
+++ brownie/test/strategies.py
@@ -54,6 +54,25 @@ def _string_strategy(min_size=0, max_size=1024):
     return st.text(min_size=min_size, max_size=max_size)
 
 
+def _array_strategy(type_str, min_length=1, max_length=8, unique=False, **kwargs):
+    abi_type = parse(type_str)
+    if len(abi_type.arrlist[-1]):
+        min_len = max_len = abi_type.arrlist[-1][0]
+    else:
+        if isinstance(min_length, list):
+            min_len = min_length.pop()
+        else:
+            min_len = min_length
+        if isinstance(max_length, list):
+            max_len = max_length.pop()
+        else:
+            max_len = max_length
+    if abi_type.item_type.is_array:
+        kwargs.update(min_length=min_length, max_length=max_length, unique=unique)
+    base_strategy = strategy(abi_type.item_type.to_type_str(), **kwargs)
+    return st.lists(base_strategy, min_len, max_len, unique=unique)
+
+
 def strategy(type_str, **kwargs):
     type_str = TYPE_STR_TRANSLATIONS.get(type_str, type_str)
     if type_str == "fixed168x10":
@@ -66,6 +85,9 @@ def strategy(type_str, **kwargs):
         return _string_strategy(**kwargs)
 
     abi_type = parse(type_str)
+    if abi_type.is_array:
+        return _array_strategy(type_str, **kwargs)
+
     base = abi_type.base
     if base in ("int", "uint"):
         return _integer_strategy(type_str, **kwargs)
