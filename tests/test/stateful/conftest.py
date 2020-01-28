2afbd4bdb5f0ee0dd0796b86aa6cfd50cd185d5d
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-01-28 15:20:32 +0400

    derandomize stateful tests to prevent flaky outcomes

--- tests/test/stateful/conftest.py
+++ tests/test/stateful/conftest.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 import pytest
+from hypothesis import settings
+
+# derandomizing prevents flaky test outcomes
+# we are testing hypothesis itself, not testing with hypothesis
+settings.register_profile("derandomize", derandomize=True)
 
 
 @pytest.fixture
 def SMTestBase(devnetwork):
+    settings.load_profile("derandomize")
+
     class _Base:
         def rule_one(self):
             pass
@@ -13,3 +20,5 @@ def SMTestBase(devnetwork):
             pass
 
     yield _Base
+
+    settings.load_profile("default")
