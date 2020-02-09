0257c11245e1fb638e213402741a2d1162b17821
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-02-09 17:10:59 +0400

    changelog, bumpversion

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ author = "Ben Hauser"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "v1.6.2"
+release = "v1.6.3"
 
 
 # -- General configuration ---------------------------------------------------
