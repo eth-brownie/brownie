6a98c7bab7dc55e80e8bf85229497deb1e2da146
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-02-20 21:27:08 +0400

    add dark stylesheet to docs

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,8 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = ["css/custom.css"]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
