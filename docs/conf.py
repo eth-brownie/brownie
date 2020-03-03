3753b0b8b8472a83cd5fdb918eae3a25680ca781
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-03-03 12:29:27 +0400

    update dependency versions, changelog, bump version to 1.6.6

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ author = "Ben Hauser"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "v1.6.5"
+release = "v1.6.6"
 
 
 # -- General configuration ---------------------------------------------------
