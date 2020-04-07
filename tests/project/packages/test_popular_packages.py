02b80aff2554606160135bd162a36c1608268b67
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-04-07 22:58:48 +0400

    test: bump aragon test version to 4.4.0

--- tests/project/packages/test_popular_packages.py
+++ tests/project/packages/test_popular_packages.py
@@ -2,7 +2,7 @@ import pytest
 
 from brownie.project.main import install_package
 
-PACKAGES = ["OpenZeppelin/openzeppelin-contracts@2.5.0", "aragon/aragonOS@4.0.0"]
+PACKAGES = ["OpenZeppelin/openzeppelin-contracts@2.5.0", "aragon/aragonOS@4.4.0"]
 
 
 @pytest.mark.parametrize("package_id", PACKAGES)
