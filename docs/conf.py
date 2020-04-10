11c511db7d84fa6ef662cd9a007b6749b55e0d23
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-04-10 15:53:39 +0400

    docs: add toggle switch for night/day mode

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,9 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_css_files = ["css/custom.css"]
+html_css_files = ["css/toggle.css", "css/dark.css"]
+
+html_js_files = ["js/toggle.js"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
