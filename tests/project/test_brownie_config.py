d9f8d5261558232c58a57a582116e23bf98a9c06
Matthias Nadler <matnad@gmail.com>
2020-05-07 21:05:57 +0200

    test: Improved and fixed config tests will only copy the brownie-config.yaml file when needed will now properly open, close and reset the network

--- tests/project/test_brownie_config.py
+++ tests/project/test_brownie_config.py
@@ -7,13 +7,13 @@ from brownie._config import _get_data_folder, _load_config
 
 
 @pytest.fixture
-def settings_proj(testproject):
-    with testproject._path.joinpath("brownie-config.yaml").open() as fp:
+def settings_proj(testprojectconfig):
+    with testprojectconfig._path.joinpath("brownie-config.yaml").open() as fp:
         settings_proj_raw = yaml.safe_load(fp)["networks"]["development"]
     yield settings_proj_raw
 
 
-def test_load_project_cmd_settings(config, testproject, settings_proj):
+def test_load_project_cmd_settings(config, testprojectconfig, settings_proj):
     """Tests if project specific cmd_settings update the network config when a project is loaded"""
     # get raw cmd_setting config data from files
     config_path_network = _get_data_folder().joinpath("network-config.yaml")
@@ -22,27 +22,30 @@ def test_load_project_cmd_settings(config, testproject, settings_proj):
     # compare initial settings to network config
     cmd_settings_network = config.networks["development"]["cmd_settings"]
     for k, v in cmd_settings_network.items():
-        assert cmd_settings_network_raw[k] == v
+        if k != "port":
+            assert cmd_settings_network_raw[k] == v
 
     # load project and check if settings correctly updated
-    testproject.load_config()
+    testprojectconfig.load_config()
     cmd_settings_proj = config.networks["development"]["cmd_settings"]
     for k, v in settings_proj["cmd_settings"].items():
-        assert cmd_settings_proj[k] == v
+        if k != "port":
+            assert cmd_settings_proj[k] == v
 
 
-def test_rpc_project_cmd_settings(network, testproject, settings_proj):
+def test_rpc_project_cmd_settings(devnetwork, testprojectconfig, config, settings_proj):
     """Test if project specific settings are properly passed on to the RPC."""
+    if devnetwork.rpc.is_active():
+        devnetwork.rpc.kill()
     cmd_settings_proj = settings_proj["cmd_settings"]
-    testproject.load_config()
-    network.connect("development")
+    testprojectconfig.load_config()
+    devnetwork.connect("development")
 
     # Check if rpc time is roughly the start time in the config file
     # Use diff < 25h to dodge potential timezone differences
-    assert cmd_settings_proj["time"].timestamp() - network.rpc.time() < 60 * 60 * 25
+    assert cmd_settings_proj["time"].timestamp() - devnetwork.rpc.time() < 60 * 60 * 25
 
-    assert cmd_settings_proj["block_time"] == network.rpc.block_time()
-    accounts = network.accounts
+    accounts = devnetwork.accounts
     assert cmd_settings_proj["accounts"] == len(accounts)
     assert cmd_settings_proj["default_balance"] == accounts[0].balance()
 
@@ -53,4 +56,5 @@ def test_rpc_project_cmd_settings(network, testproject, settings_proj):
     assert tx.gas_limit == settings_proj["gas_limit"]
     assert tx.gas_price == settings_proj["gas_price"]
 
-    assert network.rpc.evm_version() == cmd_settings_proj["evm_version"]
+    assert devnetwork.rpc.evm_version() == cmd_settings_proj["evm_version"]
+    devnetwork.rpc.kill()
