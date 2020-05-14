710c3315bc0492070a717770e28b0d961ce0b483
Ben Hauser <ben@hauser.id>
2020-05-14 13:18:04 +0400

    test: add test for account.gas_used

--- tests/network/account/test_account.py
+++ tests/network/account/test_account.py
@@ -19,3 +19,10 @@ def test_balance(accounts):
     balance = accounts[0].balance()
     assert type(balance) is Wei
     assert balance == "100 ether"
+
+
+def test_gas_paid(accounts):
+    accounts[0].transfer(accounts[1], 10000)
+    accounts[0].transfer(accounts[2], 10000)
+
+    assert accounts[0].gas_used == 42000
