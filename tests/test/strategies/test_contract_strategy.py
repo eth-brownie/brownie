35febd6ebd8488e982e51515db874bb6876aa07c
Ben Hauser <ben@hauser.id>
2020-05-19 02:05:09 +0400

    test: contract_strategy

---
+++ tests/test/strategies/test_contract_strategy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import pytest
+from hypothesis import given
+from hypothesis.strategies._internal.deferred import DeferredStrategy
+
+from brownie.network.contract import ProjectContract
+from brownie.test import contract_strategy
+
+
+def test_strategy():
+    assert isinstance(contract_strategy("ERC20"), DeferredStrategy)
+
+
+def test_repr():
+    assert repr(contract_strategy("Token")) == "sampled_from(Token)"
+
+
+def test_does_not_exist():
+    with pytest.raises(NameError):
+        contract_strategy("Foo").validate()
+
+
+@given(contract=contract_strategy("BrownieTester"))
+def test_given(tester, contract):
+    assert isinstance(contract, ProjectContract)
