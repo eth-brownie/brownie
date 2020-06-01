bd09f593b2f1e08f7d29f7b47b4631f622721f36
Ben Hauser <ben@hauser.id>
2020-06-01 14:25:05 +0400

    test: add test case for ganache mnemonic

--- tests/network/rpc/test_launch.py
+++ tests/network/rpc/test_launch.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+import brownie
 from brownie.exceptions import RPCProcessError
 
 
@@ -21,6 +22,16 @@ def test_launch(no_rpc, temp_port):
     no_rpc.launch("ganache-cli", port=temp_port)
     assert no_rpc.is_active()
     assert no_rpc.is_child()
+
+
+def test_launch_with_mnemonic(no_rpc, temp_port):
+    no_rpc.kill(False)
+    no_rpc.launch(
+        "ganache-cli",
+        port=temp_port,
+        mnemonic="patient rude simple dog close planet oval animal hunt sketch suspect slim",
+    )
+    assert brownie.network.accounts[0] == "0x7cB87a59C85a0c6d8E2953ed54f1c9E4C28E25E5"
 
 
 def test_already_active(temp_rpc, temp_port):
