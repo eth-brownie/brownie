32c8c74b65d4d421210eb0c1bf673debd6606298
Matthias Nadler <matnad@gmail.com>
2020-06-02 23:22:42 +0200

    tests: re-scope tests for performance

--- tests/network/transaction/test_required_confirmations.py
+++ tests/network/transaction/test_required_confirmations.py
@@ -1,16 +1,36 @@
 import time
+from copy import deepcopy
 
 import pytest
 
+import brownie
 
-@pytest.fixture
-def block_time_network(devnetwork, config):
+
+@pytest.fixture(scope="module")
+def block_time_config():
+    conf = brownie._config.CONFIG
+    argv = deepcopy(conf.argv)
+    networks = deepcopy(conf.networks)
+    settings = conf.settings._copy()
+    conf.networks["development"]["cmd_settings"]["block_time"] = 1
+    yield conf
+    conf.argv.clear()
+    conf.argv.update(argv)
+    conf.networks.clear()
+    conf.networks.update(networks)
+    conf.settings._unlock()
+    conf.settings.clear()
+    conf.settings.update(settings)
+    conf.settings._lock()
+
+
+@pytest.fixture(scope="module")
+def block_time_network(block_time_config):
     """Provide a network with fixed block mining time of 1 second."""
-    config.networks["development"]["cmd_settings"]["block_time"] = 1
-    devnetwork.disconnect()
-    devnetwork.connect("development")
-    yield devnetwork
-    devnetwork.disconnect()
+    if brownie.network.is_connected():
+        brownie.network.disconnect()
+    brownie.network.connect("development")
+    yield brownie.network
 
 
 def test_required_confirmations_deploy(accounts, BrownieTester, block_time_network, web3):
@@ -44,7 +64,7 @@ def test_required_confirmations_zero(accounts, block_time_network, web3):
     assert web3.eth.blockNumber - block == 0
     time.sleep(1.5)
     assert tx.status == 1
-    assert web3.eth.blockNumber - block >= 1
+    assert tx.confirmations >= 1
 
 
 def test_wait_for_confirmations(accounts, block_time_network):
