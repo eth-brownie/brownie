44816ef3e2cf17a228d15549aa59623d77e14b05
Ben Hauser <ben@hauser.id>
2020-07-30 02:08:58 +0300

    release: update changelog, bump deps, bup version to v1.10.4

--- brownie/_config.py
+++ brownie/_config.py
@@ -17,7 +17,7 @@ from hypothesis.database import DirectoryBasedExampleDatabase
 
 from brownie._singleton import _Singleton
 
-__version__ = "1.10.3"
+__version__ = "1.10.4"
 
 BROWNIE_FOLDER = Path(__file__).parent
 DATA_FOLDER = Path.home().joinpath(".brownie")
