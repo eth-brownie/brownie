ae9b21083f89ffda1c7b7486a8127df73a354b9f
Ben Hauser <ben@hauser.id>
2020-08-26 23:47:56 +0300

    feat: allow drop-in to console after successful script

--- brownie/_cli/run.py
+++ brownie/_cli/run.py
@@ -21,7 +21,7 @@ Arguments:
 Options:
   --network [name]        Use a specific network (default {CONFIG.settings['networks']['default']})
   --silent                Suppress console output for transactions
-  --interactive -I        Open an interactive console if the script fails
+  --interactive -I        Open an interactive console when the script completes or raises
   --gas -g                Display gas profile for function calls
   --tb -t                 Show entire python traceback on exceptions
   --help -h               Display this message
@@ -34,6 +34,7 @@ def main():
     args = docopt(__doc__)
     _update_argv_from_docopt(args)
 
+    active_project = None
     if project.check_for_project():
         active_project = project.load()
         active_project.load_config()
@@ -45,25 +46,33 @@ def main():
     path_str = path.absolute().as_posix()
 
     try:
-        run(args["<filename>"], method_name=args["<function>"] or "main")
+        return_value = run(args["<filename>"], method_name=args["<function>"] or "main")
+        exit_code = 0
+        extra_locals = {"_": return_value}
     except Exception as e:
         print(color.format_tb(e))
+        frame = next(
+            (i.frame for i in inspect.trace()[::-1] if Path(i.filename).as_posix() == path_str),
+            None,
+        )
+        if frame is None:
+            # exception was an internal brownie issue - do not open the console
+            sys.exit(1)
 
+        # when exception occurs in the script, open console with the namespace of the failing frame
+        exit_code = 1
+        globals_dict = {k: v for k, v in frame.f_globals.items() if not k.startswith("__")}
+        extra_locals = {**globals_dict, **frame.f_locals}
+
+    try:
         if args["--interactive"]:
-            frame = next(
-                (i.frame for i in inspect.trace()[::-1] if Path(i.filename).as_posix() == path_str),
-                None,
-            )
-            if frame is not None:
-                globals_dict = {k: v for k, v in frame.f_globals.items() if not k.startswith("__")}
+            shell = Console(active_project, extra_locals)
+            shell.interact(banner="\nInteractive mode enabled. Use quit() to close.", exitmsg="")
+    finally:
+        # the console terminates from a SystemExit - make sure we still deliver the final gas report
+        if CONFIG.argv["gas"]:
+            print("\n======= Gas profile =======")
+            for line in _build_gas_profile_output():
+                print(line)
 
-                shell = Console(active_project, {**globals_dict, **frame.f_locals})
-                shell.interact(
-                    banner="\nInteractive mode enabled. Use quit() to close.", exitmsg=""
-                )
-        sys.exit(1)
-
-    if CONFIG.argv["gas"]:
-        print("\n======= Gas profile =======")
-        for line in _build_gas_profile_output():
-            print(line)
+        sys.exit(exit_code)
