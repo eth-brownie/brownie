aea7e11660d9e8647f59e38e7a075dd1f7da863e
Ben Hauser <ben@hauser.id>
2020-11-17 04:29:43 +0400

    feat: gas strategy queue and loop

--- brownie/network/gas/bases.py
+++ brownie/network/gas/bases.py
@@ -1,9 +1,20 @@
+import threading
+import time
 from abc import ABC, abstractmethod
-from typing import Optional
+from collections import deque
+from typing import Any, Optional
+
+from brownie.network.web3 import web3
 
 
 class GasABC(ABC):
-    pass
+    def _add_tx(self, txreceipt: Any) -> None:
+        if isinstance(self, SimpleGasStrategy):
+            return
+
+        number = web3.eth.blockNumber if isinstance(self, BlockGasStrategy) else time.time()
+        _queue.append((self, txreceipt, number, number))
+        _event.set()
 
 
 class SimpleGasStrategy(GasABC):
@@ -34,3 +45,43 @@ class TimeGasStrategy(GasABC):
     @abstractmethod
     def get_gas_price(self, current_gas_price: int, elapsed_time: int) -> Optional[int]:
         raise NotImplementedError
+
+
+def _update_loop():
+    while True:
+        if not _queue:
+            _event.wait()
+            _event.clear()
+
+        try:
+            gas_strategy, tx, initial, latest = _queue.popleft()
+        except IndexError:
+            continue
+
+        if tx.status >= 0:
+            continue
+
+        if isinstance(gas_strategy, BlockGasStrategy):
+            height = web3.eth.blockNumber
+            if height - latest >= gas_strategy.block_duration:
+                gas_price = gas_strategy.get_gas_price(tx.gas_price, height - initial)
+                if gas_price is not None:
+                    tx = tx.replace(gas_price=gas_price, silent=True)
+                    latest = web3.eth.blockNumber
+
+        else:
+            if time.time() - latest >= gas_strategy.time_duration:
+                gas_price = gas_strategy.get_gas_price(tx.gas_price, time.time() - initial)
+                if gas_price is not None:
+                    tx = tx.replace(gas_price=gas_price, silent=True)
+                    latest = time.time()
+
+        _queue.append((gas_strategy, tx, initial, latest))
+        time.sleep(1)
+
+
+_queue: deque = deque()
+_event = threading.Event()
+
+_repricing_thread = threading.Thread(target=_update_loop, daemon=True)
+_repricing_thread.start()
