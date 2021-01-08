fd38f8d00e2e2008b1cd4969db75e756dcf3a3d9
matnad <matnad@gmail.com>
2021-01-08 02:48:32 +0100

    test: Added a simple test for verification info

---
+++ tests/network/contract/test_verification.py
@@ -0,0 +1,9 @@
+def test_verification_info(BrownieTester, brownie_tester_flat):
+    v = BrownieTester.get_verification_info()
+    assert v["contract_name"] == "BrownieTester"
+    assert v["compiler_version"] == "0.5.17+commit.d19bba13"
+    assert v["optimizer_enabled"] is True
+    assert v["optimizer_runs"] == 200
+    assert v["license_identifier"] == "NONE"
+    assert v["bytecode_len"] == 9842
+    assert v["flattened_source"] == brownie_tester_flat
