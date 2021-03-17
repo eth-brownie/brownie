0ba7064e9961b2c44fac0727e2f6898fb1ceeaa3
Ben Hauser <ben@hauser.id>
2021-03-17 17:47:24 +0400

    feat: geth poa middleware, brownie style

---
+++ brownie/network/middlewares/geth_poa.py
@@ -0,0 +1,21 @@
+from typing import Callable, Dict, List, Optional
+
+from web3 import Web3
+from web3.exceptions import ExtraDataLengthError
+from web3.middleware import geth_poa_middleware
+
+from brownie.network.middlewares import BrownieMiddlewareABC
+
+
+class GethPOAMiddleware(BrownieMiddlewareABC):
+    @classmethod
+    def get_layer(cls, w3: Web3, network_type: str) -> Optional[int]:
+        try:
+            w3.eth.getBlock("latest")
+            return None
+        except ExtraDataLengthError:
+            return -1
+
+    def process_request(self, make_request: Callable, method: str, params: List) -> Dict:
+        middleware_fn = geth_poa_middleware(make_request, self.w3)
+        return middleware_fn(method, params)
