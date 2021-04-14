b4745d5d5b01ea7d94bc897eb5d12f5c473bfad7
vfurci200 <10850139+vfurci200@users.noreply.github.com>
2021-04-14 10:12:55 +0200

    Fixed allow_revert error message syntax

--- brownie/network/account.py
+++ brownie/network/account.py
@@ -402,7 +402,7 @@ class _PrivateKeyAccount(PublicKeyAccount):
             msg = exc.args[0]["message"] if isinstance(exc.args[0], dict) else str(exc)
             raise ValueError(
                 f"Execution reverted during call: '{msg}'. This transaction will likely revert. "
-                "If you wish to broadcast, include `allow_revert=True` as a transaction parameter.",
+                "If you wish to broadcast, include `allow_revert:True` as a transaction parameter.",
             ) from None
 
     def deploy(
