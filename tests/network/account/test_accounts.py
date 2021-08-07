4f5c823b30ba755b6fc93542c14ae55ad2837bcf
Ben Hauser <ben@hauser.id>
2021-08-07 02:02:59 +0000

    test: target different dev networks within conftest

--- tests/network/account/test_accounts.py
+++ tests/network/account/test_accounts.py
@@ -10,7 +10,7 @@ priv_key = "0x416b8a7d9290502f5661da81f0cf43893e3d19cb9aea3c426cfb36e8186e9c09"
 addr = "0x14b0Ed2a7C4cC60DD8F676AE44D0831d3c9b2a9E"
 
 
-def test_repopulate(accounts, network, chain, rpc):
+def test_repopulate(accounts, network, chain, rpc, network_name):
     assert len(accounts) > 0
     a = list(accounts)
     chain.reset()
@@ -20,7 +20,7 @@ def test_repopulate(accounts, network, chain, rpc):
     network.disconnect()
     assert len(accounts) == 0
     assert not rpc.is_active()
-    network.connect("development")
+    network.connect(network_name)
     assert len(accounts) == len(a)
 
 
