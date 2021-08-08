b6c1f368b421d86b65dbffbd9587061de4a37a02
Ben Hauser <ben@hauser.id>
2021-08-08 21:17:56 +0000

    fix: modify hardhat config to support london ruleset

--- brownie/network/rpc/hardhat.py
+++ brownie/network/rpc/hardhat.py
@@ -21,7 +21,10 @@ HARDHAT_CONFIG = """
 module.exports = {
     networks: {
         hardhat: {
-            hardfork: "berlin",
+            hardfork: "london",
+            // base fee of 0 allows use of 0 gas price when testing
+            initialBaseFeePerGas: 0,
+            // brownie expects calls and transactions to throw on revert
             throwOnTransactionFailures: true,
             throwOnCallFailures: true
        }
