9e75a6b00a41562225a9a4590a5f324cc7dac080
Bryan Stitt <bryan@stitthappens.com>
2021-08-24 15:13:50 -0700

    Allow specifying password when saving an account (#1216)
    * Allow specifying password when saving an account
    
    * lint
    
    * more lint
    
    * add type

--- brownie/network/account.py
+++ brownie/network/account.py
@@ -842,13 +842,14 @@ class LocalAccount(_PrivateKeyAccount):
         self.public_key = eth_keys.keys.PrivateKey(HexBytes(priv_key)).public_key
         super().__init__(address)
 
-    def save(self, filename: str, overwrite: bool = False) -> str:
+    def save(self, filename: str, overwrite: bool = False, password: Optional[str] = None) -> str:
         """Encrypts the private key and saves it in a keystore json.
 
         Attributes:
             filename: path to keystore file. If no folder is given, saved in
                       ~/.brownie/accounts
             overwrite: if True, will overwrite an existing file.
+            password: Password used to encrypt the account. If none, you will be prompted
 
         Returns the absolute path to the keystore file as a string.
         """
@@ -863,9 +864,11 @@ class LocalAccount(_PrivateKeyAccount):
             json_file = Path(filename).expanduser().resolve()
         if not overwrite and json_file.exists():
             raise FileExistsError("Account with this identifier already exists")
-        encrypted = web3.eth.account.encrypt(
-            self.private_key, getpass("Enter the password to encrypt this account with: ")
-        )
+
+        if password is None:
+            password = getpass("Enter the password to encrypt this account with: ")
+
+        encrypted = web3.eth.account.encrypt(self.private_key, password)
         with json_file.open("w") as fp:
             json.dump(encrypted, fp)
         return str(json_file)
