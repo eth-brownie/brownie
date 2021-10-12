3a1c1479d07798b30ae31641914c4202b01a075e
Edward Amor <edward.amor3@gmail.com>
2021-10-12 14:58:56 -0400

    fix: add py39 classifier

--- setup.py
+++ setup.py
@@ -48,5 +48,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
