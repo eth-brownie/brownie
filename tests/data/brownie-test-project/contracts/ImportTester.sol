9db3a8768902c8da1c4919914f0af15daaf2b357
Edward Amor <edward.amor3@gmail.com>
2021-10-12 01:50:10 -0400

    fix(test): import sibling contract

--- tests/data/brownie-test-project/contracts/ImportTester.sol
+++ tests/data/brownie-test-project/contracts/ImportTester.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {ERC721PresetMinterPauserAutoId} from "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import {PackageRegistry} from "./PackageRegistry.sol";
 
-contract ImportTester is
-    ERC721PresetMinterPauserAutoId("Name", "Symbol", "BaseURI")
-{}
+contract ImportTester is PackageRegistry {}
