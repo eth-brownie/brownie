6a11cf226055a0ef70ded707680973b1dd50f94c
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-05-30 12:39:25 -0400

    chore: refactor out big ugly Literals with genexp

--- brownie/test/strategies.py
+++ brownie/test/strategies.py
@@ -15,18 +15,8 @@ TYPE_STR_TRANSLATIONS = {"byte": "bytes1", "decimal": "fixed168x10"}
 
 ArrayLengthType = Union[int, list, None]
 NumberType = Union[float, int, None]
-
-EvmIntType = Literal[
-    "int8", "int16", "int24", "int32", "int40", "int48", "int56", "int64", "int72", "int80", "int88", "int96", 
-    "int104", "int112", "int120", "int128", "int136", "int144", "int152", "int160", "int168", "int176", "int184", 
-    "int192", "int200", "int208", "int216", "int224", "int232", "int240", "int248", "int256"
-]
-
-EvmUintType = Literal[
-    "uint8", "uint16", "uint24", "uint32", "uint40", "uint48", "uint56", "uint64", "uint72", "uint80", "uint88", "uint96", 
-    "uint104", "uint112", "uint120", "uint128", "uint136", "uint144", "uint152", "uint160", "uint168", "uint176", "uint184", 
-    "uint192", "uint200", "uint208", "uint216", "uint224", "uint232", "uint240", "uint248", "uint256"
-]
+EvmIntType = Literal[tuple(f"int{i}" for i in range(8, 257, 8))]
+EvmUintType = Literal[tuple(f"uint{i}" for i in range(8, 257, 8))]
 
 class _DeferredStrategyRepr(DeferredStrategy):
     def __init__(self, fn: Callable, repr_target: str) -> None:
