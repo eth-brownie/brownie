3332ae170105d68bd2f7bd9eb7f25ea63d7efab0
Josh Davis <josh.davis@vicinft.com>
2024-11-08 08:12:34 -0500

    Fix NPE

--- brownie/network/transaction.py
+++ brownie/network/transaction.py
@@ -670,7 +670,11 @@ class TransactionReceipt:
             self._trace_exc = RPCRequestError(trace["error"]["message"])
             raise self._trace_exc
 
-        if "result" not in trace or "structLogs" not in trace["result"]:
+        if (
+            "result" not in trace
+            or trace["result"] is None
+            or "structLogs" not in trace["result"]
+        ):
             return
         
         self._raw_trace = trace = trace["result"]["structLogs"]
