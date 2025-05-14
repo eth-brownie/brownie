ba0ad15cbfae615a8ba8f21d89021ba2671a2e7c
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-14 11:14:44 -0400

    fix(test): add intermittent ConnectionError handlers to test_persistence.py

--- tests/network/contract/test_persistence.py
+++ tests/network/contract/test_persistence.py
@@ -13,11 +13,11 @@ def test_persist_load_unload(testproject, connect_to_mainnet):
 def test_delete(testproject, network, connect_to_mainnet):
     testproject.BrownieTester.at("0xdAC17F958D2ee523a2206206994597C13D831ec7")
     testproject.BrownieTester.at("0xB8c77482e45F1F44dE1745F52C74426C631bDD52")
-    network.disconnect(False)
-    network.connect("mainnet")
+    _disconnect(network)
+    _reconnect(network)
     del testproject.BrownieTester[0]
-    network.disconnect(False)
-    network.connect("mainnet")
+    _disconnect(network)
+    _reconnect(network)
     assert len(testproject.BrownieTester) == 1
     assert testproject.BrownieTester[0].address == "0xB8c77482e45F1F44dE1745F52C74426C631bDD52"
 
@@ -26,7 +26,7 @@ def test_changed_name(testproject, network, connect_to_mainnet):
     c = testproject.BrownieTester.at("0xdAC17F958D2ee523a2206206994597C13D831ec7")
     testproject.BrownieTester.at("0xB8c77482e45F1F44dE1745F52C74426C631bDD52")
     build_json = c._build
-    network.disconnect(False)
+    _disconnect(network)
     build_json["contractName"] = "PotatoTester"
 
     path = testproject._path.joinpath(
@@ -35,7 +35,22 @@ def test_changed_name(testproject, network, connect_to_mainnet):
     with path.open("w") as fp:
         json.dump(build_json, fp)
 
-    network.connect("mainnet")
+    _reconnect(network)
     assert not path.exists()
     assert len(testproject.BrownieTester) == 1
     assert testproject.BrownieTester[0].address == "0xB8c77482e45F1F44dE1745F52C74426C631bDD52"
+
+
+def _disconnect(network) -> None:
+    # Just a helper to deal with intermittent errs that aren't relevant to our tests
+    try:
+        network.disconnect(False)
+    except ConnectionError:
+        # `ConnectionError: Not connected to any network`
+        # this happens in the test runners sometimes, we're not too concerned with why or how to fix
+        pass
+
+
+def _reconnect(network) -> None:
+    # I expect an intermittent err here eventually so I'm making this handler now.
+    network.connect("mainnet")
