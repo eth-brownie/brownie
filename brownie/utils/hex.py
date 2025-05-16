a758ed50be0e96c37fdd23ddd6126b86dacebbbf
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-15 22:29:17 -0400

    Update hex.py

--- brownie/utils/hex.py
+++ brownie/utils/hex.py
@@ -10,12 +10,19 @@ HEXBYTES_LT_1_0_0: Final = tuple(int(i) for i in version("hexbytes").split("."))
     0,
     0,
 )
+"""
+A boolean constant that indicates if the version of your `hexbytes` package is less than 1.0.0.
+
+This exists to address a breaking change in hexbytes v1 and allow brownie to be used with either version.
+"""
 
 if HEXBYTES_LT_1_0_0:
     def hexstring(value: bytes) -> HexStr:
+        """Convert a bytes value to a hexstring on hexbytes<1."""
         return HexBytes(value).hex()
 
 else:
 
     def hexstring(value: bytes) -> HexStr:
+        """Convert a bytes value to a hexstring on hexbytes>=1."""
         return f"0x{HexBytes(value).hex()}"
