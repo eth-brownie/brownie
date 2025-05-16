50f68ced848c50aeb2b447600123dae7a67a843a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-15 23:41:35 -0400

    fix(test): network.connect race cond in test_mainnet

--- tests/network/test_web3.py
+++ tests/network/test_web3.py
@@ -43,9 +43,20 @@ def test_bad_env_var(web3):
 def test_mainnet(config, network, web3):
     assert type(web3._mainnet) == Web3
     assert web3._mainnet != web3
-    network.connect("mainnet")
+    try:
+        network.connect("mainnet")
+    except ConnectionError as e:
+        network.disconnect()
+        network.connect("mainnet")
+        
     assert web3._mainnet == web3
-    network.disconnect()
+    
+    try:
+        network.disconnect()
+    except ConnectionError as e:
+        if str(e) != "Not connected to any network":
+            raise
+            
     del config.networks["mainnet"]
     with pytest.raises(MainnetUndefined):
         web3._mainnet
