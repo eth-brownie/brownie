2348f6e884ec084363af84014bfd766852061242
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-21 14:40:01 -0400

    Update __init__.py

--- brownie/utils/__init__.py
+++ brownie/utils/__init__.py
@@ -4,3 +4,5 @@ from .color import Color, notify  # noqa 401
 from .hex import bytes_to_hexstring, hexbytes_to_hexstring
 
 color = Color()
+
+__all__ = ["Color", "color", "notify", "bytes_to_hexstring", "hexbytes_to_hexstring"]
