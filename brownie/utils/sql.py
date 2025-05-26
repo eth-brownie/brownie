93f332ea413e148f8d807a62ed9cd18344c1aebe
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-26 15:31:31 -0400

    Update sql.py

--- brownie/utils/sql.py
+++ brownie/utils/sql.py
@@ -3,7 +3,7 @@
 import json
 import sqlite3
 import threading
-from typing import Final, final
+from typing import Any, Final, Tuple, final
 
 
 dumps: Final = json.dumps
@@ -12,7 +12,7 @@ loads: Final = json.loads
 
 @final
 class Cursor:
-    def __init__(self, filename):
+    def __init__(self, filename: Path) -> None:
         self._lock: Final = threading.Lock()
         self._db: Final = sqlite3.connect(
             str(filename), isolation_level=None, check_same_thread=False
@@ -22,28 +22,28 @@ class Cursor:
         self._fetchone: Final = self._cur.fetchone
         self._fetchall: Final = self._cur.fetchall
 
-    def insert(self, table, *values):
+    def insert(self, table: str, *values: Any) -> None:
         values = [dumps(i) if isinstance(i, (dict, list)) else i for i in values]
         with self._lock:
             self._execute(
                 f"INSERT OR REPLACE INTO {table} VALUES ({','.join('?'*len(values))})", values
             )
 
-    def execute(self, cmd, *args):
+    def execute(self, cmd: str, *args: Any) -> None:
         with self._lock:
             self._execute(cmd, *args)
 
-    def fetchone(self, cmd, *args):
+    def fetchone(self, cmd: str, *args: Any) -> Tuple:
         with self._lock:
             self._execute(cmd, *args)
             if result := self._fetchone():
                 return tuple(loads(i) if str(i).startswith(("[", "{")) else i for i in result)
 
-    def fetchall(self, cmd, *args):
+    def fetchall(self, cmd: str, *args: Any) -> Any:
         with self._lock:
             self._execute(cmd, *args)
             return self._fetchall()
 
-    def close(self):
+    def close(self) -> None:
         self._cur.close()
         self._db.close()
