5287fdda606d22986bb2b7bce8e5353cfeb8233d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-28 02:51:23 -0400

    feat(mypy): network constant types (#1880)
    * feat(mypy):
    
    this PR should make the types visible downstream ie in downstream repos importing them from the main `__init__.py`
    
    * chore: `black .` and `pip-compile`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- brownie/network/__init__.py
+++ brownie/network/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from typing import Final
 
 from .account import Accounts
 from .main import (  # NOQA 401
@@ -19,7 +20,7 @@ from .web3 import web3
 __all__ = ["accounts", "chain", "history", "rpc", "web3"]
 __console_dir__ = ["connect", "disconnect", "show_active", "is_connected", "gas_limit", "gas_price"]
 
-accounts = Accounts()
-rpc = Rpc()
-history = TxHistory()
-chain = Chain()
+accounts: Final[Accounts] = Accounts()
+rpc: Final[Rpc] = Rpc()
+history: Final[TxHistory] = TxHistory()
+chain: Final[Chain] = Chain()
