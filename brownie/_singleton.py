50960c134e6e0e49165bd415f7e355959a37d2d1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-05 10:46:55 -0400

    Update _singleton.py

--- brownie/_singleton.py
+++ brownie/_singleton.py
@@ -7,6 +7,8 @@ class _Singleton(type):
     _instances: Dict = {}
 
     def __call__(cls, *args, **kwargs):
+        # NOTE counterintuitively, when you supply type hints for a metaclass call method,
+        #      it breaks the standard typing for the created instances. Do not add types here.
         if cls not in cls._instances:
             cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
