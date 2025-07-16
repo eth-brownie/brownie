3168c27b4deec1292d85cd846c093bb49e5b3599
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-16 19:32:05 -0400

    Update vyper.py

--- brownie/project/compiler/vyper.py
+++ brownie/project/compiler/vyper.py
@@ -61,13 +61,27 @@ def set_vyper_version(version: Union[str, Version]) -> str:
     return str(_active_version)
 
 
-def get_abi(contract_source: str, name: str) -> Dict:
+class SourceJSON(TypedDict):
+    content: str
+
+Sources = Dict[ContractName, SourceJSON]
+
+class SettingsJSON(TypedDict):
+    outputSelection: Dict[str, Dict[str, List[str]]]
+
+class InputJSON(TypedDict, total=False):
+    language: Language
+    sources: Sources
+    settings: SettingsJSON
+    
+
+def get_abi(contract_source: str, name: ContractName) -> Dict[ContractName, List[Dict[str, Any]]]:
     """
     Given a contract source and name, returns a dict of {name: abi}
 
     This function is deprecated in favor of `brownie.project.compiler.get_abi`
     """
-    input_json = {
+    input_json: InputJSON = {
         "language": "Vyper",
         "sources": {name: {"content": contract_source}},
         "settings": {"outputSelection": {"*": {"*": ["abi"]}}},
@@ -220,7 +234,7 @@ def find_best_vyper_version(
 
 
 def compile_from_input_json(
-    input_json: Dict, silent: bool = True, allow_paths: Optional[str] = None
+    input_json: InputJSON, silent: bool = True, allow_paths: Optional[str] = None
 ) -> Dict:
     """
     Compiles contracts from a standard input json.
@@ -261,22 +275,34 @@ def _get_unique_build_json(
 ) -> Dict:
 
     ast: List = ast_json["body"] if isinstance(ast_json, dict) else ast_json
+    deployed_bytecode: dict = output_evm["deployedBytecode"]
     pc_map, statement_map, branch_map = _generate_coverage_data(
-        output_evm["deployedBytecode"]["sourceMap"],
-        output_evm["deployedBytecode"]["opcodes"],
+        deployed_bytecode["sourceMap"],
+        deployed_bytecode["opcodes"],
         contract_name,
         ast,
     )
+    bytecode_dct: dict = output_evm["bytecode"]
+    bytecode: HexStr = bytecode["object"]
     return {
         "allSourcePaths": {"0": path_str},
-        "bytecode": output_evm["bytecode"]["object"],
-        "bytecodeSha1": sha1(output_evm["bytecode"]["object"].encode()).hexdigest(),
+        "bytecode": bytecode,
+        "bytecodeSha1": sha1(bytecode.encode()).hexdigest(),
         "coverageMap": {"statements": statement_map, "branches": branch_map},
         "dependencies": _get_dependencies(ast),
         "offset": offset,
         "pcMap": pc_map,
         "type": "contract",
     }
+
+
+class AstObject(TypedDict):
+    """A dictionary representing on object on the AST."""
+    name: str
+    module: str
+    type: str
+
+AstJson = List[AstObject]
 
 
 def _get_dependencies(ast_json: List[dict]) -> List[str]:
@@ -290,7 +316,18 @@ def _get_dependencies(ast_json: List[dict]) -> List[str]:
     )
 
 
-def _is_revert_jump(pc_list: List[dict], revert_pc: int) -> bool:
+class ProgramCounter(TypedDict):
+    op: str
+    value: str
+    count: int
+    fn: str
+    offset: NotRequired[List[int]]
+    dev: NotRequired[str]
+    optimizer_revert: NotRequired[Literal[True]]
+
+PcList = List[ProgramCounter]
+
+def _is_revert_jump(pc_list: PcList, revert_pc: int) -> bool:
     return pc_list[-1]["op"] == "JUMPI" and int(pc_list[-2].get("value", "0"), 16) == revert_pc
 
 
@@ -310,7 +347,7 @@ def _generate_coverage_data(
     statement_map: Dict = {}
     branch_map: Dict[str, Dict] = {}
 
-    pc_list: List = []
+    pc_list: PcList = []
     count, pc = 0, 0
 
     revert_pc = -1
@@ -325,81 +362,83 @@ def _generate_coverage_data(
 
         # format of source is [start, stop, contract_id, jump code]
         source = source_map.popleft()
-        pc_list.append({"op": opcodes.popleft(), "pc": pc})
+        op = opcodes.popleft()
+        this = {"op": op, "pc": pc}
+        pc_list.append(this)
 
         if source[3] != "-":
-            pc_list[-1]["jump"] = source[3]
+            this["jump"] = source[3]
 
         pc += 1
         if opcodes and opcodes[0][:2] == "0x":
-            pc_list[-1]["value"] = opcodes.popleft()
-            pc += int(pc_list[-1]["op"][4:])
+            this["value"] = opcodes.popleft()
+            pc += int(op[4:])
 
         # set source offset (-1 means none)
         if source[0] == -1:
             if (
                 len(pc_list) > 6
                 and pc_list[-7]["op"] == "CALLVALUE"
-                and pc_list[-1]["op"] == "REVERT"
+                and op == "REVERT"
             ) or (
                 len(pc_list) > 2
                 and pc_list[-3]["op"] == "CALLVALUE"
                 and _is_revert_jump(pc_list[-2:], revert_pc)
             ):
                 # special case - initial nonpayable check on vyper >=0.2.5
-                pc_list[-1]["dev"] = "Cannot send ether to nonpayable function"
+                this["dev"] = "Cannot send ether to nonpayable function"
                 # hackiness to prevent the source highlight from showing the entire contract
-                if pc_list[-1]["op"] == "REVERT":
+                if op == "REVERT":
                     # for REVERT, apply to the previous opcode
                     pc_list[-2].update(path="0", offset=[0, 0])
                 else:
                     # for JUMPI we need the mapping on the actual opcode
-                    pc_list[-1].update(path="0", offset=[0, 0])
+                    this.update(path="0", offset=[0, 0])
             continue
-        offset = (source[0], source[0] + source[1])
-        pc_list[-1]["path"] = "0"
-        pc_list[-1]["offset"] = offset
+
+        this["path"] = "0"
+        this["offset"] = offset = (source[0], source[0] + source[1])
 
         try:
             if "offset" in pc_list[-2] and offset == pc_list[-2]["offset"]:
-                pc_list[-1]["fn"] = pc_list[-2]["fn"]
+                this["fn"] = pc_list[-2]["fn"]
             else:
                 # statement coverage
                 fn = next(k for k, v in fn_offsets.items() if is_inside_offset(offset, v))
-                pc_list[-1]["fn"] = f"{contract_name}.{fn}"
+                this["fn"] = f"{contract_name}.{fn}"
                 stmt_offset = next(i for i in stmt_nodes if is_inside_offset(offset, i))
                 stmt_nodes.remove(stmt_offset)
-                statement_map.setdefault(pc_list[-1]["fn"], {})[count] = stmt_offset
-                pc_list[-1]["statement"] = count
+                statement_map.setdefault(this["fn"], {})[count] = stmt_offset
+                this["statement"] = count
                 count += 1
         except (KeyError, IndexError, StopIteration):
             pass
 
-        if pc_list[-1]["op"] not in ("JUMPI", "REVERT"):
+        if op not in ("JUMPI", "REVERT"):
             continue
 
         node = _find_node_by_offset(ast_json, offset)
         if node is None:
             continue
 
-        if pc_list[-1]["op"] == "REVERT" or _is_revert_jump(pc_list[-2:], revert_pc):
+        if op == "REVERT" or _is_revert_jump(pc_list[-2:], revert_pc):
             # custom revert error strings
             if node["ast_type"] == "FunctionDef":
-                if (pc_list[-1]["op"] == "REVERT" and pc_list[-7]["op"] == "CALLVALUE") or (
-                    pc_list[-1]["op"] == "JUMPI" and pc_list[-3]["op"] == "CALLVALUE"
+                if (op == "REVERT" and pc_list[-7]["op"] == "CALLVALUE") or (
+                    op == "JUMPI" and pc_list[-3]["op"] == "CALLVALUE"
                 ):
-                    pc_list[-1]["dev"] = "Cannot send ether to nonpayable function"
+                    this["dev"] = "Cannot send ether to nonpayable function"
             elif node["ast_type"] == "Subscript":
-                pc_list[-1]["dev"] = "Index out of range"
+                this["dev"] = "Index out of range"
             elif node["ast_type"] in ("AugAssign", "BinOp"):
-                if node["op"]["ast_type"] == "Sub":
-                    pc_list[-1]["dev"] = "Integer underflow"
+                if this["ast_type"] == "Sub":
+                    this["dev"] = "Integer underflow"
                 elif node["op"]["ast_type"] == "Div":
-                    pc_list[-1]["dev"] = "Division by zero"
+                    this["dev"] = "Division by zero"
                 elif node["op"]["ast_type"] == "Mod":
-                    pc_list[-1]["dev"] = "Modulo by zero"
+                    this["dev"] = "Modulo by zero"
                 else:
-                    pc_list[-1]["dev"] = "Integer overflow"
+                    this["dev"] = "Integer overflow"
             continue
 
         if node["ast_type"] in ("Assert", "If") or (
@@ -407,18 +446,20 @@ def _generate_coverage_data(
             and node["value"].get("func", {}).get("id", None) == "assert_modifiable"
         ):
             # branch coverage
-            pc_list[-1]["branch"] = count
-            branch_map.setdefault(pc_list[-1]["fn"], {})
+            this["branch"] = count
+            this_fn = this["fn"]
+            branch_map.setdefault(this_fn, {})
             if node["ast_type"] == "If":
-                branch_map[pc_list[-1]["fn"]][count] = _convert_src(node["test"]["src"]) + (False,)
+                branch_map[this_fn][count] = _convert_src(node["test"]["src"]) + (False,)
             else:
-                branch_map[pc_list[-1]["fn"]][count] = offset + (True,)
+                branch_map[this_fn][count] = offset + (True,)
             count += 1
 
-    pc_list[0]["path"] = "0"
-    pc_list[0]["offset"] = [0, _convert_src(ast_json[-1]["src"])[1]]
+    first = pc_list[0]
+    first["path"] = "0"
+    first["offset"] = [0, _convert_src(ast_json[-1]["src"])[1]]
     if revert_pc != -1:
-        pc_list[-1]["optimizer_revert"] = True
+        this["optimizer_revert"] = True
 
     pc_map = {i.pop("pc"): i for i in pc_list}
 
@@ -433,17 +474,18 @@ def _convert_src(src: str) -> Tuple[int, int]:
 
 
 def _find_node_by_offset(ast_json: List[Dict], offset: Tuple[int, int]) -> Optional[Dict]:
-    for node in [i for i in ast_json if is_inside_offset(offset, _convert_src(i["src"]))]:
-        if _convert_src(node["src"]) == offset:
-            return node
-        node_list = [i for i in node.values() if isinstance(i, dict) and "ast_type" in i]
-        node_list.extend([x for i in node.values() if isinstance(i, list) for x in i])
-        if node_list:
-            result = _find_node_by_offset(node_list, offset)
-        else:
-            result = _find_node_by_offset(ast_json[ast_json.index(node) + 1 :], offset)
-        if result is not None:
-            return result
+    for node in ast_json:
+        if is_inside_offset(offset, _convert_src(i["src"])):
+            if _convert_src(node["src"]) == offset:
+                return node
+            node_list = [i for i in node.values() if isinstance(i, dict) and "ast_type" in i]
+            node_list.extend((x for i in node.values() if isinstance(i, list) for x in i))
+            if node_list:
+                result = _find_node_by_offset(node_list, offset)
+            else:
+                result = _find_node_by_offset(ast_json[ast_json.index(node) + 1 :], offset)
+            if result is not None:
+                return result
     return None
 
 
