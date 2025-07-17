467c2430116e593d550569749cafcd3003ac0959
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-17 18:56:35 -0400

    Update typing.py

--- brownie/typing.py
+++ brownie/typing.py
@@ -1,4 +1,7 @@
-from typing import TypeVar
+from typing import List, Literal, TypedDict, TypeVar, final
+
+from eth_event.main import EventData
+from eth_typing import ChecksumAddress
 
 from brownie.network.account import Accounts
 from brownie.network.transaction import TransactionReceipt
@@ -6,6 +9,14 @@ from brownie.network.transaction import TransactionReceipt
 # NETWORK
 # Account
 AccountsType = TypeVar("AccountsType", bound=Accounts)
+
+# Event
+@final
+class FormattedEvent(TypedDict):
+    name: str | Literal["(anonymous)", "(unknown)"]
+    data: List[EventData]
+    decoded: bool
+    address: ChecksumAddress
 
 # Transactions
 TransactionReceiptType = TypeVar("TransactionReceiptType", bound=TransactionReceipt)
