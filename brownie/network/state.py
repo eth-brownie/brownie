f215d0f33bf3f0c0155a1dc5c3ebf9f840541ab1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-18 18:26:30 +0000

    Update state.py

--- brownie/network/state.py
+++ brownie/network/state.py
@@ -7,7 +7,7 @@ import weakref
 from hashlib import sha1
 from pathlib import Path
 from sqlite3 import OperationalError
-from typing import TYPE_CHECKING, Any, Callable, Dict, Iterator, List, Optional, Tuple, Union, final
+from typing import TYPE_CHECKING, Any, Callable, Dict, Final, Iterator, List, Optional, Tuple, Union, final
 
 from eth_typing import BlockNumber
 from eth_utils.toolz import keymap
