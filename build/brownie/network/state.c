058956978c128808f4ab53957013273dee85e640
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-07-18 00:32:55 +0000

    chore: compile

---
+++ build/brownie/network/state.c
@@ -0,0 +1,21 @@
+#include <Python.h>
+
+PyMODINIT_FUNC
+PyInit_state(void)
+{
+    PyObject *tmp;
+    if (!(tmp = PyImport_ImportModule("67ba59a4b46c20ffa2b1__mypyc"))) return NULL;
+    PyObject *capsule = PyObject_GetAttrString(tmp, "init_brownie___network___state");
+    Py_DECREF(tmp);
+    if (capsule == NULL) return NULL;
+    void *init_func = PyCapsule_GetPointer(capsule, "67ba59a4b46c20ffa2b1__mypyc.init_brownie___network___state");
+    Py_DECREF(capsule);
+    if (!init_func) {
+        return NULL;
+    }
+    return ((PyObject *(*)(void))init_func)();
+}
+
+// distutils sometimes spuriously tells cl to export CPyInit___init__,
+// so provide that so it chills out
+PyMODINIT_FUNC PyInit___init__(void) { return PyInit_state(); }
