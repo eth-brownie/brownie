71fc678cdf80bad6a1d392310b1980d624acf93f
NV <95428380+nvy-0x@users.noreply.github.com>
2025-07-19 22:54:44 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ else:
             [
                 "brownie/_cli",
                 "brownie/_expansion.py",
+                "brownie/exceptions.py",
                 "brownie/convert/__init__.py",
                 "brownie/convert/main.py",
                 "brownie/convert/normalize.py",
