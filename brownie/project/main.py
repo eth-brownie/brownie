cc6bf28e08c515c8a5d72ab88e5761dda139212e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-19 01:48:12 +0000

    Update main.py

--- brownie/project/main.py
+++ brownie/project/main.py
@@ -620,7 +620,9 @@ def new(
 
 
 def from_brownie_mix(
-    project_name: str, project_path: Optional[Path | str] = None, ignore_subfolder: bool = False
+    project_name: str,
+    project_path: Optional[Path | str] = None,
+    ignore_subfolder: bool = False,
 ) -> str:
     """Initializes a new project via a template. Templates are downloaded from
     https://www.github.com/brownie-mix
