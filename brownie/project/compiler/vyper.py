e514cbc220e6ef4eb35b21d5d8314fab8d57fe98
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-05 19:40:42 +0000

    Update vyper.py

--- brownie/project/compiler/vyper.py
+++ brownie/project/compiler/vyper.py
@@ -25,7 +25,6 @@ from brownie.project.sources import is_inside_offset
 from brownie.typing import (
     ContractName,
     InputJsonVyper,
-    Language,
     Offset,
     PcList,
     ProgramCounter,
