1e6fb227f0471dd7a284d4a758cd4a2f61c9e2e9
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-19 04:11:01 +0000

    chore: compile

--- build/__native_internal_b5e4b1180acefab623dd.h
+++ build/__native_internal_b5e4b1180acefab623dd.h
@@ -6,7 +6,7 @@
 
 int CPyGlobalsInit(void);
 
-extern PyObject *CPyStatics[2452];
+extern PyObject *CPyStatics[2447];
 extern const char * const CPyLit_Str[];
 extern const char * const CPyLit_Bytes[];
 extern const char * const CPyLit_Int[];
