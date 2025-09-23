d33036ef36eb4f35e6ae97b6ebb7534ed8057639
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-22 23:02:00 -0400

    Update state.py

--- brownie/network/state.py
+++ brownie/network/state.py
@@ -349,7 +349,7 @@ class Chain(metaclass=_Singleton):
     ) -> None:
         with self._undo_lock:
             tx._confirmed.wait()
-            self._undo_buffer.append((self._current_id, fn, args, kwargs))
+            self._undo_buffer.append((self._current_id, fn, args, kwargs))  # type: ignore [arg-type]
             redo_buffer = self._redo_buffer
             if redo_buffer and (fn, args, kwargs) == redo_buffer[-1]:
                 redo_buffer.pop()
