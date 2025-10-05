bed59a1c49c947787b7f093d80eeac4c4e8611a3
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-04 21:45:47 -0400

    fix mypy err

--- brownie/project/compiler/__init__.py
+++ brownie/project/compiler/__init__.py
@@ -331,7 +331,7 @@ def generate_build_json(
 
     for path_str, path_contracts in contracts.items():
         if path_str in sources:
-            source: str = sources[path_str]["content"]
+            source: str = sources[path_str]["content"]  # type: ignore [typeddict-item]
             get_alias = False
         else:
             with Path(path_str).open(encoding="utf-8") as fp:
