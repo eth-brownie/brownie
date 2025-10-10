c288ec5ebaf9d79a477765cb382c7d563905e1d7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-10 19:59:36 +0000

    Update runner.py

--- brownie/test/managers/runner.py
+++ brownie/test/managers/runner.py
@@ -3,14 +3,14 @@
 import builtins
 import sys
 import warnings
-from pathlib import Path
+from typing import Dict
 
 import pytest
 from _pytest._io import TerminalWriter
 from eth_utils.toolz import keyfilter
 
 import brownie
-from brownie._c_constants import regex_compile, regex_fullmatch, ujson_dump
+from brownie._c_constants import Path, regex_compile, regex_fullmatch, ujson_dump
 from brownie._cli.console import Console
 from brownie._config import CONFIG
 from brownie.exceptions import VirtualMachineError
